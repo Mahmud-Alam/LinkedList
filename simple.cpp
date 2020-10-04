2dcb8b7e0c55db44bdaec3d13a3aa059e381c2a8
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-05 01:32:15 +0600

    simple linkedlist code added

---
+++ simple.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node(int value){
+        this->value = value;
+        this->next = NULL;
+    }
+};
+void printList(Node* head){
+    Node* p = head;
+    while(p!=NULL){
+        cout<<p->value<<" ";
+        p=p->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* head = new Node(5);
+    Node* head2 = new Node(1);
+    Node* head3 = new Node(2);
+    head->next = head2;
+    head2->next = head3;
+
+    printList(head);
+}
