87932b4eb0aacb441214a8fa6fdf82d460b33c7e
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-05 15:21:12 +0600

    int node header file

---
+++ nodeInt.h
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(){
+        next = NULL;
+    }
+    Node(int value){
+        data = value;
+        next = NULL;
+    }
+    ~Node();
+};
+
+void printList(Node* head){
+    Node* p = head;
+    while(p!=NULL){
+        if(p->next!=NULL)
+            cout<<p->data<<" -> ";
+        else
+            cout<<p->data;
+
+        p = p->next;
+    }
+    cout<<endl;
+}
