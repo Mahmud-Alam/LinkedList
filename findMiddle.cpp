adb1c1dbc883817401b9cd8eec98414ee31cd5e2
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-08 23:33:38 +0600

    findMiddle Function done

--- findMiddle.cpp
+++ findMiddle.cpp
@@ -21,3 +21,25 @@ int findMiddle(Node* head){
         p = p->next;
     }
 }
+
+int main(){
+    int v;
+    Node* head = NULL;
+    while(1){
+        cout<<"Input a value(1024 to stop): ";cin>>v;
+        if(v==1024)
+            break;
+        else{
+            Node* newHead = createLinkedList(v,head);
+            head = newHead;
+            displayList(head);
+        }
+    }
+    if(head==NULL) cout<<endl<<"list is empty!"<<endl;
+    else if(head->next==NULL) cout<<endl<<"List has only one Node!\nFinding Middle Node is not possible!"<<endl;
+    else if(head->next->next==NULL) cout<<endl<<"List has only two Node!\nFinding Middle Node is not possible!"<<endl;
+    else{
+        int mid = findMiddle(head);
+        cout<<endl<<"Linked List's Middle Node is: "<<mid<<endl;
+    }
+}
