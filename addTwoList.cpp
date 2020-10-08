91ec8b80441647ed1b0832a332b6bebd6c0c13b3
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-08 23:40:34 +0600

    Add two list

---
+++ addTwoList.cpp
@@ -0,0 +1,13 @@
+#include"LinkedList.h"
+
+//__________________ADD two Linked List Function_______________//
+
+
+Node* addTwoList(Node* head, Node* head2){
+    Node* p = head;
+    while(p!=NULL){
+        p = p->next
+    }
+    p->next = head2;
+    return head;
+}
