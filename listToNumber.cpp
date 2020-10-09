1ce844477f40cc658e2c590fbbf02ed67682541e
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-09 14:58:48 +0600

    list to number

---
+++ listToNumber.cpp
@@ -0,0 +1,16 @@
+#include"LinkedList.h"
+
+int main(){
+    int v;
+    Node* head = NULL;
+    while(1){
+        cout<<"Input a value(1024 to stop): ";cin>>v;
+        if(v==1024)
+            break;
+        else{
+            Node* newHead = createLinkedList(v,head);
+            head = newHead;
+            displayList(head);
+        }
+    }
+}
