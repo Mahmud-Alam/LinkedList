6d246d9bdb9264c81280dce6d79deb477c20f964
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-10 21:23:36 +0600

    done

--- findOddEvenNodesFromList.cpp
+++ findOddEvenNodesFromList.cpp
@@ -13,3 +13,33 @@ int findEvenNodes(Node* head){
     }
     return c;
 }
+int findLength(Node* head){
+    Node* p = head;
+    int c = 0;
+    while(p!=NULL){
+        c++;
+        p = p->next;
+    }
+    return c;
+}
+int main(){
+    int v;
+    Node* head = NULL;
+    while(1){
+        cout<<"Input a value(1024 to stop): ";cin>>v;
+        if(v==1024)
+            break;
+        else{
+            Node* newHead = createLinkedList(v,head);
+            head = newHead;
+            displayList(head);
+        }
+    }
+    int even = findEvenNodes(head);
+    int length = findLength(head);
+    int odd = length - even;
+
+    cout<<endl<<"List length: "<<length<<endl;
+    cout<<endl<<"Even Nodes : "<<even<<endl;
+    cout<<endl<<"Odd Nodes  : "<<odd<<endl;
+}
