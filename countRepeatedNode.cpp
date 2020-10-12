cfc622d8b14e46d0c97816e60330375f95e95f6c
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-12 19:15:03 +0600

    nodes are repeated in display too :(

--- countRepeatedNode.cpp
+++ countRepeatedNode.cpp
@@ -1,5 +1,31 @@
 #include"LinkedList.h"
 
+//___________Count Repeated Nodes Function__________//
+
+
+int countRepeatedNode(int key, Node* head){
+    int c=0;
+    Node* p = head;
+    while(p->next!=NULL){
+        if(p->data == key)
+            c++;
+        p = p->next;
+    }
+    if(p->data == key)
+        c++;
+    return c;
+}
+
+void displayRepeatedNode(Node* head){
+    Node* p = head;
+    cout<<endl<<"___All Node's repeated numbers___"<<endl<<endl;
+    while(p!=NULL){
+        cout<<p->data<<" is : "<<countRepeatedNode(p->data,head)<<" times."<<endl;
+        p = p->next;
+    }
+    cout<<endl;
+}
+
 int main(){
     int v,pos;
     Node* head = NULL;
@@ -16,6 +42,6 @@ int main(){
     if(head == NULL)
         cout<<endl<<"List is empty!"<<endl;
     else{
-
+        displayRepeatedNode(head);
     }
 }
