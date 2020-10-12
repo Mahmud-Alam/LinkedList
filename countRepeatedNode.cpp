26f98057b9fb7f4a45ef934ea1d7f56c20a73d03
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-12 18:45:02 +0600

    -

---
+++ countRepeatedNode.cpp
@@ -0,0 +1,21 @@
+#include"LinkedList.h"
+
+int main(){
+    int v,pos;
+    Node* head = NULL;
+    while(1){
+        cout<<"Input a value(1024 to stop): ";cin>>v;
+        if(v==1024)
+            break;
+        else{
+            Node* newHead = createLinkedList(v,head);
+            head = newHead;
+            displayList(head);
+        }
+    }
+    if(head == NULL)
+        cout<<endl<<"List is empty!"<<endl;
+    else{
+
+    }
+}
