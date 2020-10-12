4bbcacbae0b90f5c445c47652337ba3c95aaf7f1
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-12 14:53:14 +0600

    func and main

--- popByPosition.cpp
+++ popByPosition.cpp
@@ -9,10 +9,13 @@ Node* popByPosition(int pos, Node*head){
     if(p->next==NULL){
         if(pos==c){
             free(p);
+            cout<<endl<<"Pop is done! Now list is empty."<<endl;
             return NULL;
         }
-        else
+        else{
             cout<<endl<<"Position is not found!"<<endl;
+            return NULL;
+        }
     }
     else if(p->next->next == NULL){
         if(pos==c){
@@ -24,8 +27,10 @@ Node* popByPosition(int pos, Node*head){
             free(p->next);
             return p;
         }
-        else
+        else{
             cout<<endl<<"Position is not found!"<<endl;
+            return NULL;
+        }
     }
     while(p->next->next!=NULL){
         if(pos == c){
@@ -34,7 +39,7 @@ Node* popByPosition(int pos, Node*head){
             return temp;
         }
         c++;
-        else if(pos == c){
+        if(pos == c){
             Node* temp = p->next->next;
             free(p->next);
             p->next = temp;
@@ -51,7 +56,42 @@ Node* popByPosition(int pos, Node*head){
         p->next = NULL;
         return head;
     }
-    else
+    else{
         cout<<endl<<"Position is not found!"<<endl;
+        return NULL;
+        }
+    }
+}
+int main(){
+    int v,pos;
+    Node* head = NULL;
+    while(1){
+        cout<<"Input a value(1024 to stop): ";cin>>v;
+        if(v==1024)
+            break;
+        else{
+            Node* newHead = createLinkedList(v,head);
+            head = newHead;
+            displayList(head);
+        }
+    }
+    if(head == NULL)
+        cout<<endl<<"List is empty!"<<endl;
+    else{
+        cout<<endl<<"Do you want to pop any node by index number?(If YES type 1, if NO type 0)";cin>>v;
+        while(1){
+            if(v==0)
+                break;
+            else if(v==1){
+                cout<<endl<<"Input a index: ";cin>>pos;
+                Node* newHead = popByPosition(pos,head);
+                if(head!=NULL)
+                    displayList(head);
+            }
+            else{
+                cout<<endl<<"Wrong keyword! Type again."<<endl;
+            }
+            cout<<endl<<"Do you want to pop any node by index number Again?(If YES type 1, If NO type 0)";cin>>v;
+        }
     }
 }
