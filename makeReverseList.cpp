10c4b78367a04f246bc0387f0eef744c2465cab3
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-14 00:07:05 +0600

    not done

--- makeReverseList.cpp
+++ makeReverseList.cpp
@@ -1,1 +1,36 @@
+#include"LinkedList.h"
 
+//__________Make Reverse Linked List__________//
+
+
+Node* makeReverseList(Node* head){
+    if(head!=NULL && head->next!=NULL)
+        return head;
+    Node* rest = makeReverseList(head->next);
+
+    head->next->next = head;
+    head->next = NULL;
+
+    return rest;
+}
+
+int main(){
+    int v,pos;
+    Node* head = NULL;
+    while(1){
+        cout<<"Input a value(1024 to stop): ";cin>>v;
+        if(v==1024)
+            break;
+        else{
+            Node* newHead = createLinkedList(v,head);
+            head = newHead;
+            displayList(head);
+        }
+    }
+    if(head == NULL)
+        cout<<endl<<"List is empty!"<<endl;
+    else{
+        head = makeReverseList(head);
+        displayList(head);
+    }
+}
