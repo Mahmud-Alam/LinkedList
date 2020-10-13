dfeb0868732f09bb326d98047e5d0d72f82d7947
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-13 20:31:02 +0600

    replaced last node to first node

---
+++ replacedLastNodeToFirstNode.cpp
@@ -0,0 +1,40 @@
+#include"LinkedList.h"
+
+//__________Replaced Last Node to First Node Function__________//
+
+
+Node* replacedLastNodeToFirstNode(Node* head){
+    Node* p = head;
+    if(p->next==NULL)
+        return head;
+    else{
+        while(p->next->next!=NULL){
+            p = p->next;
+        }
+        Node* temp  = p->next;
+        p->next = NULL;
+        temp->next = head;
+        return temp;
+    }
+}
+int main(){
+    int v,pos;
+    Node* head = NULL;
+    while(1){
+        cout<<"Input a value(1024 to stop): ";cin>>v;
+        if(v==1024)
+            break;
+        else{
+            Node* newHead = createLinkedList(v,head);
+            head = newHead;
+            displayList(head);
+        }
+    }
+    if(head == NULL)
+        cout<<endl<<"List is empty!"<<endl;
+    else{
+        Node* newHead = replacedLastNodeToFirstNode(head);
+        displayList(newHead);
+    }
+}
+
