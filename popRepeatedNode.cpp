21cc7522ba74de72ba243f13c4294cf51f3ad55a
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-13 16:22:44 +0600

    pop repeated node ERROR solved with vivid explanation

--- popRepeatedNode.cpp
+++ popRepeatedNode.cpp
@@ -1,32 +1,29 @@
 #include"LinkedList.h"
 
+//||||||||||||||||||||Error solved and detail explanation|||||||||||||||||||||||||||||||||||
+
 //____________Pop Repeated Nodes_____________//
 
 Node* popRepeatedNodes(Node* head){
     Node* p = head;
     if(p->next==NULL)
         return head;
-    else if(p->next->next==NULL){
-        if(p->data==p->next->data){
-            free(p->next);
-            p->next = NULL;
-        }
-        return head;
-    }
     else{
-        while(p->next->next!=NULL){
-            int key = p->data;
-            Node* q = p->next;
-            while(q->next!=NULL){
-                if(q->data == key){
-                    Node* temp = q->next;
-                    free(q);
-                    p->next = temp;
+        while(p!=NULL && p->next!=NULL){             //p!=NULL na dile(1->1->1->1) = (1->NULL) hoye 2nd loop theke ber hoy
+            Node* q = p;                            //But 1st loop er moddhe condition(1->next!=NULL) ERROR hoy,karon ekhane (1->next==NULL) hbe.
+            while(q->next!=NULL){                  //tai condition (P!=NULL) o dite hbe, jeno 1st loop ERROR na hoye ber hote pare.
+                if(p->data == q->next->data){
+                    Node* temp = q->next->next;
+                    free(q->next);
+                    q->next = temp;
                 }
-                q = q->next;
+                else                             //else use na kre, only (q = q->next) dile (1->1->1->1->2) = (1->1->2) ata hoy.
+                    q = q->next;                //fully duplicate delete hoy na. karon shift hoye jae tai akta duplicate theke jay.
             }
+            p = p->next;
         }
     }
+    return head;
 }
 
 int main(){
@@ -45,6 +42,7 @@ int main(){
     if(head == NULL)
         cout<<endl<<"List is empty!"<<endl;
     else{
-
+        cout<<endl<<"_____After Popping All Duplicate Nodes______"<<endl;
+        displayList(popRepeatedNodes(head));
     }
 }
