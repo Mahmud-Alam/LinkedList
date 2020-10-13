27db7f39ecb87ab9f4320e861fff162a85825e40
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-14 00:10:24 +0600

    done

--- replacedLastNodeToFirstNode.cpp
+++ replacedLastNodeToFirstNode.cpp
@@ -18,7 +18,7 @@ Node* replacedLastNodeToFirstNode(Node* head){
     }
 }
 int main(){
-    int v,pos;
+    int v;
     Node* head = NULL;
     while(1){
         cout<<"Input a value(1024 to stop): ";cin>>v;
